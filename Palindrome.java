7731973a0d68d2dd187fb58f97b69c930be59410
takticode <sagiv0611@gmail.com>
2025-12-29 11:19:37 +0200

    final submission Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,20 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		if(s.length() <= 1){
+		return true;
     }
+
+	char first = s.charAt(0);
+	char last = s.charAt(s.length()-1);
+
+	if(first != last){
+		return false;
+	}
+
+	String middle = s.substring(1, s.length()-1);
+
+	return isPalindrome(middle);
+}
+
 }
